b225ec05edce3121ec378c712f65691fae992872
vertoker <vertogpro@gmail.com>
2023-09-18 21:38:33 +0500

    begin to understand

---
+++ RenderHierarchyLib/Models/Transform2D.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Xna.Framework;
+
+namespace RenderHierarchyLib.Models
+{
+    public struct Transform2D
+    {
+        private Vector2 _pos;
+        private float _rot;
+        private Vector2 _sca;
+
+        public Transform2D()
+        {
+            _pos = Vector2.Zero;
+            _rot = 0;
+            _sca = Vector2.One;
+        }
+    }
+}
---
+++ RenderHierarchyLib/Models/TransformCamera2D.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Xna.Framework;
+
+namespace RenderHierarchyLib.Models
+{
+    public struct TransformCamera2D
+    {
+        private Vector2 _pos;
+        private float _rot;
+        private float _sca;
+
+        public TransformCamera2D(float pixelScale = 2)
+        {
+            _pos = Vector2.Zero;
+            _rot = 0;
+            _sca = pixelScale;
+        }
+    }
+}
