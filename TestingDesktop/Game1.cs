2dc18f148ed736e9208609355c571c762012bd8a
vertoker <vertogpro@gmail.com>
2023-09-24 19:41:44 +0500

    refactoring

--- TestingDesktop/Game1.cs
+++ TestingDesktop/Game1.cs
@@ -30,8 +30,6 @@ namespace TestingDesktop
 
         protected override void Initialize()
         {
-            // TODO: Add your initialization logic here
-
             base.Initialize();
         }
 
@@ -39,7 +37,7 @@ namespace TestingDesktop
         {
             _spriteBatch = new SpriteBatch(GraphicsDevice);
             _hierarchySpriteBatch = new HierarchyRenderBatch(GraphicsDevice, _camera);
-            _texture1 = new TextureView2D(Content.Load<Texture2D>("Test"));
+            _texture1 = new TextureView2D(Content.Load<Texture2D>("Test"), new(0, 0.5f), new(1f, 1f));
             _texture2 = new TextureView2D(Content.Load<Texture2D>("Test2"));
             _texture3 = new TextureView2D(Content.Load<Texture2D>("Test3"));
 
@@ -60,7 +58,7 @@ namespace TestingDesktop
         {
             GraphicsDevice.Clear(Color.CornflowerBlue);
 
-            var counter = (float)gameTime.TotalGameTime.TotalSeconds * 300;
+            var counter = (float)gameTime.TotalGameTime.TotalSeconds * 50;
 
             _hierarchySpriteBatch.Begin();
             
@@ -73,16 +71,17 @@ namespace TestingDesktop
             _hierarchySpriteBatch.Render(_texture3, new() { Anchor = AnchorPresets.CenterBottom, Rot = counter });
             _hierarchySpriteBatch.Render(_texture3, new() { Anchor = AnchorPresets.RightBottom, Rot = counter });
 
-            var parent = new RenderObject() { Anchor = AnchorPresets.CenterMiddle, Pivot = AnchorPresets.LeftBottom, Pos = new(0, 0), Rot = counter, Sca = new(2, 2) };
-            _hierarchySpriteBatch.Render(_texture2, parent);
+            var parent = new RenderObject() { Anchor = AnchorPresets.CenterMiddle, Pivot = AnchorPresets.LeftTop, 
+                Pos = new(0, 0), Rot = counter, Sca = new(2f, 1f) };
+            _hierarchySpriteBatch.Render(_texture1, parent);
 
             var child = new RenderObject() { Anchor = AnchorPresets.CenterMiddle, Pivot = AnchorPresets.RightTop, Pos = new(0.5f, 0.5f), Rot = counter };
             var child2 = child.SetParentNewTransform(parent);
-            _hierarchySpriteBatch.Render(_texture2, child2);
+            _hierarchySpriteBatch.Render(_texture1, child2);
             var child3 = child.SetParentNewTransform(child2);
-            _hierarchySpriteBatch.Render(_texture2, child3);
+            _hierarchySpriteBatch.Render(_texture1, child3);
             var child4 = child.SetParentNewTransform(child3);
-            _hierarchySpriteBatch.Render(_texture2, child4);
+            _hierarchySpriteBatch.Render(_texture1, child4);
 
             _hierarchySpriteBatch.End();
 
